8885f5591cc198e0a928758473f41c5847639e41
wilun <wilun.dawid@itismeucci.com>
2022-03-03 10:30:04 +0100

    inserito nome

--- stampa.cpp
+++ stampa.cpp
@@ -1,7 +1,7 @@
 /*
 Scrivere un programma che stampi "n" volte la stringa "Bada come la fuma"  con "n" dato in input dall'utente.
 
-NOME COGNOME DATA
+dawid wilun 03.03.22
 */
 
 # include <iostream>
